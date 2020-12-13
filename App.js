0502f6aeca834794cbad6e446f421097c0a613da
Yosuke Yamaguchi <yonosho456@gmail.com>
2020-12-13 22:42:09 +0900

    1st commit.

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import ListItem from "./components/ListItem";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <ListItem
+        author="SampleNews"
+        title="はい、Webの開発経験はあるけどアプリの開発は初めてという方、エンジニアになりたての人にお薦めのReact
+        Native入門講座。2019年に導入されたHooksの記法にも対応。React
+        Native現場歴3年の講師が基礎から丁寧に解説します。"
+        imageUrl="https://picsum.photos/id/10/200/200"
+      />
+      <ListItem
+        author="SampleNews2"
+        title="2Webの開発経験はあるけどアプリの開発は初めてという方、エンジニアになりたての人にお薦めのReact
+        Native入門講座。2019年に導入されたHooksの記法にも対応。React
+        Native現場歴3年の講師が基礎から丁寧に解説します。"
+        imageUrl="https://picsum.photos/200/200"
+      />
+      <ListItem
+        author="SampleNews3"
+        title="3Webの開発経験はあるけどアプリの開発は初めてという方、エンジニアになりたての人にお薦めのReact
+        Native入門講座。2019年に導入されたHooksの記法にも対応。React
+        Native現場歴3年の講師が基礎から丁寧に解説します。"
+        imageUrl="https://picsum.photos/id/12/200/200"
+      />
+    </View>
+  );
+}
