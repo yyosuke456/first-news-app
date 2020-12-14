fcaf43ef66ce5590e9c3ad64f4be953ec8c30276
Yosuke Yamaguchi <yonosho456@gmail.com>
2020-12-14 23:17:47 +0900

    jsonの値を取得してリスト表示。useState, useEffectを使ってみる

--- App.js
+++ App.js
@@ -1,40 +1,37 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, FlatList, SafeAreaView } from "react-native";
 import ListItem from "./components/ListItem";
+import dummyArticles from "./dummies/articles.json";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
 
 export default function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setArticles(dummyArticles);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <ListItem
-        author="SampleNews"
-        title="はい、Webの開発経験はあるけどアプリの開発は初めてという方、エンジニアになりたての人にお薦めのReact
-        Native入門講座。2019年に導入されたHooksの記法にも対応。React
-        Native現場歴3年の講師が基礎から丁寧に解説します。"
-        imageUrl="https://picsum.photos/id/10/200/200"
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
       />
-      <ListItem
-        author="SampleNews2"
-        title="2Webの開発経験はあるけどアプリの開発は初めてという方、エンジニアになりたての人にお薦めのReact
-        Native入門講座。2019年に導入されたHooksの記法にも対応。React
-        Native現場歴3年の講師が基礎から丁寧に解説します。"
-        imageUrl="https://picsum.photos/200/200"
-      />
-      <ListItem
-        author="SampleNews3"
-        title="3Webの開発経験はあるけどアプリの開発は初めてという方、エンジニアになりたての人にお薦めのReact
-        Native入門講座。2019年に導入されたHooksの記法にも対応。React
-        Native現場歴3年の講師が基礎から丁寧に解説します。"
-        imageUrl="https://picsum.photos/id/12/200/200"
-      />
-    </View>
+    </SafeAreaView>
   );
 }
